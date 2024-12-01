7e9d6c75546eab1fbcd9f9bc37dc773fc456504e
kaushalsatam <kaushalabd17@gmail.com>
2024-12-01 23:45:58 +0530

    added morgan package

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import router from "./routes/routes.js";
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(cors());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 // routes
 app.use('/', router)
